790cf1abb24dd2235ce2e601c0e19ca4d423c345
Naik <SN035704@cerner.net>
2017-02-15 21:19:16 +0530

    Changed hungry.py. changed readme

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # learn_git
 To learn using of git hub commands 
 By trying out some examples
+This is the second edition to read me file.
--- hungry.py
+++ hungry.py
@@ -1,1 +1,8 @@
-print ("I am hungry")
+
+choice=input("are you hungry? yes/no")
+
+
+if choice == "yes":
+    print ("I am hungry")
+else:
+    print("I am not hungry!!!")
